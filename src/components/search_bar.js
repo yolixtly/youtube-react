44b8de4eb0c70f7b6d06a2cf82946e9e8f0999b3
yolixtly <yolixtly_13@hotmail.com>
2017-04-17 20:18:03 -0400

    OnInputChange added to SearchBar and JSDocs included too

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 
 
+/**
+ * Renders the SearchBar for the App
+ */
 class SearchBar extends Component {
+
+
+  /**
+   * onInputChange - description
+   *
+   * @param  {type} event - describes the value of the input element
+   * @return {type}       description
+   *
+   * @memberof SearchBar
+   */
+  onInputChange(event){
+    console.log(event);
+  }
+
+  /**
+   * render - description
+   *
+   * @return {type}  - the html content for the SearchBar
+   *
+   * @memberof SearchBar
+   */
   render() {
     return (
-      <input />
+      <div className="container-fluid row">
+        <input className="col-sm-12" onChange={this.onInputChange}/>
+      </div>
     );
   }
 }
